bba23259baf932295bf0f9ee639e646d5a2cc301
razevedomil <117126406+razevedomil@users.noreply.github.com>
2023-02-14 19:26:32 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MyParallax from './components/parallax/Parallax'
+import NavBar from './components/nav/NavBar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar />
+      <MyParallax />
+    </div>
+  )
+}
+
+export default App
